c5f862cfc3268de9eebf0d4203aeb164daefd371
justinhluo <33584797+justinhluo@users.noreply.github.com>
2025-06-17 23:33:16 -0700

    added input validation for signs and fixed bug when deleting signs

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 let firstNum = "";
 let secondNum = "";
 let equation = "";
+let signEntered = false; // checks if sign in equation, if true and another sign inputted, call operate
 function output(input){
     const display = document.querySelector(".input");
     if(input == "clear" || input == "Escape") {
         display.textContent = "";
         equation = "";
+        secondNum = "";
+        equation = "";
     }else if(input == "delete" || input == "Backspace") {
-      display.textContent = display.textContent.slice(0,-1);
+      display.textContent = display.textContent.trim().slice(0,-1);
       equation = equation.slice(0, -1);
     }else if(input == '+' || input == '-' ||input == '/' ||input == '*') {
-      display.append(` ${input} `);
-      equation += input;
+      if(!parseInt(equation[equation.length-1])) { //if last input is sign, change sign instead of adding another sign
+        display.textContent = display.textContent.trim().slice(0, -1) + ` ${input} `;
+        equation = equation.slice(0, -1) + input;
+      }else if (!signEntered) { //if sign hasn't been entered in equation add sign 
+        display.append(` ${input} `);
+        equation += input;
+      }else { //if sign is already in equation and last input wasn't sign call operate and add sign to result
+        //operate
+      }
+      
     }else if(input == '=' || input == 'Enter') {
       //operate()
     }
@@ -19,9 +30,7 @@ function output(input){
         display.append(input);
         equation += input;
     }
-    
 }
-
 
 
 document.addEventListener("keydown", (e) => {
@@ -42,5 +51,5 @@ document.querySelectorAll("button").forEach(button => {
 
 //if equation is empty, first char MUST be digit, decimal, or -
 //
-
+//console.log(parseInt('s'));
 
